a7242bd9b7875380fe71b6d402ba7c3abde8b8d1
ecmascriptguru <ecmascript.guru@gmail.com>
2017-04-18 04:10:09 -0400

    bootstrap datatable integrated

--- js/options.js
+++ js/options.js
@@ -1,3 +1,15 @@
-﻿(function(window, jQuery) {
+﻿var Options = (function() {
+	var _dataTable = null,
+		init = function() {
+			_dataTable = $('#data-table').DataTable();
+		};
+
+	return {
+		init: init
+	}
+})();
+
+(function(window, jQuery) {
 	console.log("Options page is working.");
+	Options.init();
 })(window, $);
