3f722f262ec77efc788ccc844806c9df2190a506
ecmascriptguru <ecmascript.guru@gmail.com>
2017-04-18 05:29:39 -0400

    citation change event handler created

--- js/options.js
+++ js/options.js
@@ -8,7 +8,7 @@
 			for (var i = 0; i < _data.length; i ++) {
 				var row = [i + 1];
 				row.push(_data[i].title);
-				row.push("<div class='col-xs-12'><textarea class='form-control'>" + _data[i].citation + "</textarea></div>");
+				row.push("<div class='col-xs-12'><textarea data-id='" + i + "' class='form-control citation'>" + _data[i].citation + "</textarea></div>");
 				row.push(_data[i].copied_at);
 				row.push("<div class=''>" +
 					"<button class='action-copy btn btn-default col-xs-6' data-id='" + i + "'>Copy</button>" +
@@ -18,8 +18,20 @@
 				_dataTable.row.add(row).draw();
 			}
 		},
+
+		changeData = function(index, citation) {
+			var _data = JSON.parse(localStorage._saved_data || "[]");
+			_data[index].citation = citation;
+			localStorage._saved_data = JSON.stringify(_data);
+		},
+
 		init = function() {
 			_dataTable = $('#data-table').DataTable();
+			$("#data-table tbody").on("change", "textarea.citation", function() {
+				var index = parseInt($(this).attr('data-id')),
+					citation = $(this).val();
+				changeData(index, citation);
+			});
 			drawTable();
 		};
 
